a5d9603d573048308a68f63726d2e19cec8847f7
Roman Kesler <roman@cybellum.com>
2021-11-21 07:40:29 -0800

    Delete zero days

--- main-app.c
+++ main-app.c
@@ -9,89 +9,55 @@
 
 void download_video()
 {
-	char* ptr = (char*)malloc(sizeof(char));
-	free(ptr);
-	free(ptr);
+
 }
 
 void _close_connection(char* ptr)
 {
-	if (rand() % 2 == 0)
-		free(ptr);
+
 }
 
 void close_connection()
 {
-	char* ptr = (char*)malloc(10 * sizeof(char));
-	int i;
-	for (i = 0; i < 5; i++)
-	{
-		*(ptr + i) = 'a';
-	}
 
-	if (rand() % 2 == 0)
-	{
-		_close_connection(ptr);
-	}
-
-	if (rand() % 2 == 0)
-		_close_connection(ptr);
 }
 
 void calculate_md5()
 {
-	char* ptr = "a";
-	free(ptr);
+
 }
 
 void rename_video()
 {
-	char* buf1 = (char*)calloc(5, sizeof(char));
-	char* buffer2 = (char*)calloc(5, sizeof(char));
-	char** pbuf[2] = { &buf1, &buffer2 };
-	int i, j = 1;
-	if (buffer2 != NULL)
-	{
-		buffer2[0] = 10.0;
-	}
-	for (i = 0; i < 2; i++)
-	{
-		*((*pbuf[i]) + j) = 5.0;
-	}
-	free(buf1);
-	free(buffer2);
-	free(pbuf);
+
 }
 
 void release_video_buffer()
 {
-	char ptr[0x256];
-	gets(ptr);
+
 }
 
 int release_audio_buffer()
 {
-	char* buffer = (char*)malloc(20);
+	char* buffer = (char*)malloc(21);
 	int size = 21;
 	return (int)memset(buffer, '*', size);
 }
 
 void copy_local_audio_file(char* str)
 {
-	char* copy_str = strdup("USERNAME");
-	strcat(copy_str, str);
+
 }
 
 void init_new_song()
 {
-	char* ptr = (char*)malloc(0x256);
-	gets(ptr);
+
 }
 
 int start_init_routine()
 {
 	char buffer[4];
-	int size = 50;
+	int size = 4;
 	return (int)memset(buffer, '*', size);
 }
 
@@ -104,94 +70,54 @@ int _report_signals(int a, int b, int c, char* buffer, int size, char* buffer2)
 int report_signals()
 {
 	char buffer[20];
-	char buffer2[21];
-	return _report_signals(1, 5, 9, buffer, 100, buffer2);
+	char buffer2[20];
+	return _report_signals(1, 5, 9, buffer, 20, buffer2);
 }
 
 int critical_control_monitor()
 {
 	char* buffer1 = (char*)malloc(20);
 	char buffer2[20];
-	int result = (int)strncpy(buffer1, buffer2, 20);
+	int result = (int)strncpy(buffer1, buffer2, 10);
 	return result;
 }
 
 void update_fw_version()
 {
-	char* env = getenv("USERNAME");
-	printf("Value is: %s\n", env);
 }
 
 void read_config(char* str2)
 {
-	char* str = strstr("STRING", str2);
-	printf("Value is: %s\n", str);
 }
 
 void restart_services(char* format2)
 {
-	FILE* file;
-	char* format = "%s";
-	char* buffer;
-	if (rand() % 2)
-	{
-		int result = fscanf(file, format, buffer);
-	}
-	else
-	{
-		int result = fscanf(file, format2, buffer);
-	}
+
 }
 
 int interval_check()
 {
-	FILE* fp;
-	char* ptr = (char*)malloc(0x1000);
-	if (ptr != NULL)
-	{
-		fread(ptr, 10, sizeof(ptr), fp);
-	}
+
 	return 8;
 }
 
 int fw_validation_process(char* buffer)
 {
-	char* new_buffer = buffer;
-	free(new_buffer);
-	*new_buffer = '\a';
+
 }
 
 void debug_event_report()
 {
-	int* ptr, a;
-	int flag = 10;
 
-	(flag == 10) ? (ptr = (int*)malloc(10 * sizeof(int))) : (a = 5);
-
-	if (ptr != NULL)
-	{
-		(flag == 10) ? (*(ptr + 1) = 10) : (a = 100);
-
-		if (flag == 10)
-		{
-			free(ptr);
-		}
-	}
-	if (flag == 10)
-		a = *(ptr + 1);
 }
 
 void establish_client_connection()
 {
-	char* dummy;
-	char* buffer = (char*)malloc(100);
-	memset(buffer, '*', sizeof(dummy));
+
 }
 
 int connect_new_server()
 {
-	char* ptr = (char*)malloc(0x1000);
-	recv(0x12324, ptr, 0x1000, 0);
 	return 5;
 }
 
@@ -215,30 +141,30 @@ int main(void){
   }
 
   curl_easy_cleanup(curl);
-char* buffer = (char*)malloc(0x100);
-*buffer = 'a';
-*(buffer + 1) = 0;
-int result;
+  char* buffer = (char*)malloc(0x100);
+  *buffer = 'a';
+  *(buffer + 1) = 0;
+  int result;
 
-download_video();
-close_connection();
-calculate_md5();
-rename_video();
-release_video_buffer();
-release_audio_buffer();
-copy_local_audio_file(buffer);
-init_new_song();
-start_init_routine();
-report_signals();
-critical_control_monitor();
-update_fw_version();
-read_config(buffer);
-restart_services(buffer);
-interval_check();
-fw_validation_process(buffer);
-debug_event_report();
-establish_client_connection();
-connect_new_server();
+  download_video();
+  close_connection();
+  calculate_md5();
+  rename_video();
+  release_video_buffer();
+  release_audio_buffer();
+  copy_local_audio_file(buffer);
+  init_new_song();
+  start_init_routine();
+  report_signals();
+  critical_control_monitor();
+  update_fw_version();
+  read_config(buffer);
+  restart_services(buffer);
+  interval_check();
+  fw_validation_process(buffer);
+  debug_event_report();
+  establish_client_connection();
+  connect_new_server();
 
   return 0;
 }
